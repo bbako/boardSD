e606e00c34e6519baa0dda0ed4088e47ce7b5ab3
hoyeon <hoyeon1008@gmail.com>
2017-10-17 17:22:10 +0900

    hy

--- boardSD/src/main/java/org/board/persistence/BoardDAOImpl.java
+++ boardSD/src/main/java/org/board/persistence/BoardDAOImpl.java
@@ -2,21 +2,21 @@ package org.board.persistence;
 
 import javax.inject.Inject;
 
+import org.apache.ibatis.session.SqlSession;
 import org.board.domain.BoardVO;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO {
 
 	@Inject
-	SqlSessionTemplate sess;
+	SqlSession sess;
 	
 	String namespace = "org.board.persistence.BoardDAO";
 	
 	@Override
 	public void create(BoardVO vo) {
-			sess.insert(namespace+".create");
+			sess.insert(namespace+".create", vo);
 	}
 
 }
